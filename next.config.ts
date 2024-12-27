3951a0fac9643440b1321cada1756515cd8757aa
Jessica Chen <lalalanmao10@gmail.com>
2024-12-27 02:01:32 -0600

    initial spotify try

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.API_URL
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async rewrites() {
+		return [
+			{
+				source: '/auth/:path*',
+				destination: `${API_URL}/:path*`,
+			},
+		]
+	},
 };
 
 export default nextConfig;
